5d08112e632ed218e4947b5f410485c954f9dc44
slashrawr <brandon.michael.lewis@gmail.com>
2025-03-08 13:26:26 +0800

    added cards and refactored lambda

--- lambda/main.py
+++ lambda/main.py
@@ -1,51 +1,52 @@
 import os
 from datetime import date
+from urllib.request import Request, urlopen
 import requests
 import json
 import boto3
 
-# Env variables
+s3 = boto3.resource('s3')
+    
+# Bedrock client used to interact with APIs around models
+bedrock = boto3.client(
+    service_name='bedrock', 
+    region_name='us-east-1'
+)
+    
+# Bedrock Runtime client used to invoke and question the models
+bedrock_runtime = boto3.client(
+    service_name='bedrock-runtime', 
+    region_name='us-east-1'
+)
+
 MODEL_TYPE = os.environ['model_type']
 NYT_API = os.environ['nyt_api']
 NYT_API_KEY = os.environ['nyt_api_key']
 NYT_QUERY = os.environ['nyt_query']
 NYT_NEWS_DESK = os.environ['nyt_news_desk']
 NYT_TYPE_OF_MATERIAL = os.environ['nyt_type_of_material']
-PROMPT = os.environ['prompt']
 APP_ID = os.environ['app_id']
 BRANCH = os.environ['branch']
 S3_BUCKET = os.environ['s3_bucket']
 S3_KEY_DATA_FILE = os.environ['s3_key_data_file']
-DEPLOY_ONLY = os.environ['deploy_only']
 
-s3 = boto3.resource('s3')
-    
-bedrock = boto3.client(
-    service_name='bedrock', 
-    region_name='us-east-1'
-)
-    
-bedrock_runtime = boto3.client(
-    service_name='bedrock-runtime', 
-    region_name='us-east-1'
-)
+PROMPT = "You are an unbiased political analyst. You carefully analyse news articles every day as part of chronicling President Donald J. Trump's 2nd term. Below is a collection of articles from today. Summarise each article into a single sentence highlighting actions and activities by Donald J. Trump, Elon Musk and his administration that are ethically questionable, potentially illegal, undermine American institutions and American constitution or would otherwise be considered \"dumb\" by any sensible individual. It is vital you are as objective as possible and do not embellish or read into details that are not there. Only include articles that are directly related to President Trump, Elon Musk or his administration. If there is nothing improper, then ignore the article. Where articles describe the same thing, combine them into a single summary. Phrase summaries in the third person. Cite the respective article URL or set of URLs if articles are combined along with the summary. Provide an assessment of severity (LOW, MED, HIGH) of the implications of the actions or activities reported in the articles. Identify a list of key \"tags\" for each entry. Leave out the preamble and postamble. Return the summaries in JSON format with the following schema: [ { \"summary\" : <summary>, \"urls\" : [<urls>], \"severity\" : <severity>, \"tags\" : [<tags>] } ]"
 
 def call_nyt():
     try:
         current_date = date.today().strftime("%Y%m%d")
-        query_params = {
-            "api-key": NYT_API_KEY,
-            "begin_date": current_date,
-            "end_date": current_date,
-            "q": NYT_QUERY,
-            "news_desk": NYT_NEWS_DESK,
-            "type_of_material": NYT_TYPE_OF_MATERIAL
-        }
+        query_params = {"api-key": NYT_API_KEY,
+                   "begin_date": current_date,
+                   "end_date": current_date,
+                   "q": NYT_QUERY,
+                   "news_desk": NYT_NEWS_DESK,
+                   "type_of_material": NYT_TYPE_OF_MATERIAL}
         response = requests.get(NYT_API, params=query_params)
-        response.raise_for_status()
+        response.raise_for_status()  # Raise an error for bad status codes (4xx, 5xx)
         articles = []
 
         for item in response.json()["response"]["docs"]:
+            print(json.dumps(item) + "\n")
             article = {"url": item["web_url"], "abstract": item["abstract"] + item["lead_paragraph"]}
             articles.append(article)           
 
@@ -69,10 +70,10 @@ def call_mistral(articles):
     )
      
     response = bedrock_runtime.invoke_model(
-        body=body, 
-        modelId='mistral.mixtral-8x7b-instruct-v0:1', 
-        accept='application/json', 
-        contentType='application/json'
+    body=body, 
+    modelId='mistral.mixtral-8x7b-instruct-v0:1', 
+    accept='application/json', 
+    contentType='application/json'
     )
     
     response_body = json.loads(response.get('body').read())
@@ -93,22 +94,17 @@ def call_claude(articles):
             "messages": [
                 {
                     "role": "user",
-                    "content": [
-                        {
-                            "type": "text", 
-                            "text": PROMPT + article_text
-                        }
-                    ],
+                    "content": [{"type": "text", "text": PROMPT + article_text}],
                 }
             ]
         }
     )
     
     response = bedrock_runtime.invoke_model(
-        body=body, 
-        modelId='arn:aws:bedrock:us-east-1:959425594836:inference-profile/us.anthropic.claude-3-5-haiku-20241022-v1:0', 
-        accept='application/json', 
-        contentType='application/json'
+    body=body, 
+    modelId='arn:aws:bedrock:us-east-1:959425594836:inference-profile/us.anthropic.claude-3-5-haiku-20241022-v1:0', 
+    accept='application/json', 
+    contentType='application/json'
     )
     
     response_body = json.loads(response.get('body').read())
@@ -119,29 +115,38 @@ def read_from_s3():
     content_object = s3.Object(S3_BUCKET, S3_KEY_DATA_FILE)
     file_content = content_object.get()['Body'].read().decode('utf-8')
     return json.loads(file_content)
+    print(data)
 
 def write_to_s3(new_data):
     content_object = s3.Object(S3_BUCKET, S3_KEY_DATA_FILE)
-    content_object.put(Body=json.dumps(new_data))
+    content_object.put(Body=json.dumps(new_data), ACL='public-read')
 
 def add_to_data(current_data, new_data):
     for element in current_data:
         new_data.append(element)
     write_to_s3(new_data)
 
+
 def lambda_handler(event, context):
+    data = read_from_s3()
+    nyt_response = call_nyt()
+    model_response = None
+
+    if MODEL_TYPE == 'claude':
+        model_response = call_claude(nyt_response)
+    elif MODEL_TYPE == 'mistral':
+        model_response = call_mistral(nyt_response)
+
+    print(model_response)
+    add_to_data(data, json.loads(model_response[0]['text']))
+
     try:
-        data = read_from_s3()
-        nyt_response = call_nyt()
-        model_response = None
-
-        if MODEL_TYPE == 'claude':
-            model_response = call_claude(nyt_response)
-        elif MODEL_TYPE == 'mistral':
-            model_response = call_mistral(nyt_response)
-
-        add_to_data(data, json.loads(model_response[0]['text']))
-
         print("Job done.")
+        #if model_response is None:
+        #    raise ValueError('model_response empty')
     except:
         raise
+    else:
+        return event['time']
+    finally:
+        print("Run complete.")
